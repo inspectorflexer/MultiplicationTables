51f1506d1a89735da3dcb3c511e6884f67a7d69e
inspectorflexer <inspectorflexer@gmail.com>
2019-04-25 22:00:54 +0100

    Number validation added to make sure no letters or invalid integers are used in user input, do while loop added to keep program running till a valid number is selected, number of sets finctionality implemented

--- MultiplicationTables/Program.cs
+++ MultiplicationTables/Program.cs
@@ -2,23 +2,48 @@
 
 namespace MultiplicationTables
 {
-    internal class Program
+    internal static class Program
     {
+        private static int? _multiplyNumber;
+        private static int? _numberOfSets;
+
         private static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the number that you want and this console app will generate the times table until 12.");
-            int inputNumber = Convert.ToInt32(Console.ReadLine());
-            GenerateTimesTable(inputNumber);
+            do
+            {
+                Console.WriteLine(
+                    "Please enter the number that you want and this console app will generate the times table until a given point");
+                _multiplyNumber = Console.ReadLine().ValidateNumber();
+            } while (_multiplyNumber == null);
+
+            do
+            {
+                Console.WriteLine("Please enter the number of sets you'd like to go up to");
+                _numberOfSets = Console.ReadLine().ValidateNumber();
+            } while (_numberOfSets == null);
+
+            GenerateTimesTable((int)_multiplyNumber, (int)_numberOfSets);
+
             Console.ReadLine();
         }
 
-        private static void GenerateTimesTable(int number)
+        private static void GenerateTimesTable(int multiplyNumber, int numberOfSets)
         {
             Console.WriteLine("These your generated times table!!!");
-            for (int i = 0; i <= 12; i++)
+            for (int i = 0; i <= numberOfSets; i++)
             {
-                Console.WriteLine($"{number} * {i} = {number * i}");
+                Console.WriteLine($"{multiplyNumber} * {i} = {multiplyNumber * i}");
             }
+        }
+
+        public static int? ValidateNumber(this string str)
+        {
+            bool success = int.TryParse(str, out int number);
+            if (success)
+                return number;
+
+            Console.WriteLine("Valid number not provided");
+            return null;
         }
 
     }
